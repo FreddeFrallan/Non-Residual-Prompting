69fc0d2abe4df6e2433121823831010e92bac5db
Fredrik Carlsson <FreddeFc@gmail.com>
2022-04-29 16:25:17 +0200

    Model Load Paths

--- Utils.py
+++ Utils.py
@@ -7,14 +7,18 @@ def loadPaperGPT2LargeModel():
     maxSeqLen, maxPromptLen = 32, 128
     base = 'gpt2-large'
     # promptModelPath = 'PaperModel-GPT2Large-PromptModel'
-    promptModelPath = 'Non-Residual-Prompting/GPT2-Large'
+    # promptModelPath = 'Non-Residual-Prompting/GPT2-Large'
+    clmBasePath = 'PaperModel-GPT2Large-CLM'
+    promptModelBasePath = 'PaperModel-GPT2Large-PromptModel'
     postWeightsPath = 'PaperModel-GPT2Large-PostWeights'
+    # postWeightsPath = None
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(base)
-    model = PromptModelSetup.PromptModelSetup(base, promptModelWeightsPath=promptModelPath,
+    model = PromptModelSetup.PromptModelSetup(base, CLMWeightsPath=clmBasePath,
+                                              promptModelWeightsPath=promptModelBasePath,
                                               postWeightsPath=postWeightsPath)
-    promptGenerator = PromptGenerator.PromptGenerator(tokenizer, maxPromptLen)
 
+    promptGenerator = PromptGenerator.PromptGenerator(tokenizer, maxPromptLen)
     return model, tokenizer, maxSeqLen, promptGenerator
 
 
