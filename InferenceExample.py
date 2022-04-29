31a5981237e4e240db2014510d3f502b1d60cc4a
Fredrik Carlsson <FreddeFc@gmail.com>
2022-04-29 16:13:21 +0200

    Inference Example

--- InferenceExample.py
+++ InferenceExample.py
@@ -1,8 +1,7 @@
-import os
-
-GPU = input("GPU:")
-os.environ["CUDA_VISIBLE_DEVICES"] = str(GPU)
-
+# import os
+#
+# GPU = input("GPU:")
+# os.environ["CUDA_VISIBLE_DEVICES"] = str(GPU)
 from NonResidualAttention import InferenceUtils
 from DecodingStrategies import WordInclusion
 import Utils
@@ -15,13 +14,10 @@ if __name__ == '__main__':
         ['wikileaks', 'scandal', 'sweden', 'website'],
     ]
 
-    # # List of ints for the tokenized prompt for each sequence. Will be padded later, so does not have to be same length
     promptIDs = [promptGenerator.createPromptIDs(words, sentLen) for words in targetWords]
     contextLen = len(tokenizer.encode(context))
     WordInclusion.setCurrentInclusionWords(targetWords, sentLen, contextLen=contextLen, numBeams=numBeams)
 
-    # The context string for each sequence, HAS TO BE SAME LENGTH
-    # Will later be replaced with <StartText>
     sampleContexts = [context for _ in targetWords]
     result = InferenceUtils.batchGeneration(model, tokenizer, sampleContexts, promptIDs, seqLen, numBeams)
     for txt, words in zip(result, targetWords):
--- Utils.py
+++ Utils.py
@@ -6,8 +6,8 @@ import transformers
 def loadPaperGPT2LargeModel():
     maxSeqLen, maxPromptLen = 32, 128
     base = 'gpt2-large'
-    promptModelPath = 'PaperModel-GPT2Large-PromptModel'
-    # promptModelPath = 'Non-Residual-Prompting/GPT2-Large'
+    # promptModelPath = 'PaperModel-GPT2Large-PromptModel'
+    promptModelPath = 'Non-Residual-Prompting/GPT2-Large'
     postWeightsPath = 'PaperModel-GPT2Large-PostWeights'
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(base)
